b59b0be80463bfcf507dd21af742fc297b18263a
viljinsky <viljinsky@narod.ru>
2022-05-30 12:48:44 +0300

    2020 05 30 hello java

---
+++ src/ru/viljinsky/hello/HelloJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ru.viljinsky.hello;
+
+/**
+ *
+ * @author viljinsky
+ */
+public class HelloJava {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+    }
+    
+}
